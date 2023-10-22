605c0cba50a47b8abd1f2cdb6b98b5bb70edcfab
print("Ayush") <142672894+AyushmanNP05@users.noreply.github.com>
2023-10-22 19:09:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,213 @@
+// Hent knapper og niveauer fra HTML
+const level1 = document.getElementById("level1");
+const level2 = document.getElementById("level2");
+const level3 = document.getElementById("level3");
+const level4 = document.getElementById("level4");
+const level5 = document.getElementById("level5");
+const level6 = document.getElementById("level6");
+const level7 = document.getElementById("level7");
+const level8 = document.getElementById("level8");
+const level9 = document.getElementById("level9");
+const level10 = document.getElementById("level10");
+const level11 = document.getElementById("level11");
+const level12 = document.getElementById("level12");
+const level13 = document.getElementById("level13");
+const level14 = document.getElementById("level14");
+const level15 = document.getElementById("level15");
+const level16 = document.getElementById("level16");
+const level17 = document.getElementById("level17");
+const level18 = document.getElementById("level18");
+const level19 = document.getElementById("level19");
+const level20 = document.getElementById("level20");
+const level21 = document.getElementById("level21");
+const level22 = document.getElementById("level22");
+const level23 = document.getElementById("level24");
+const level24 = document.getElementById("level24");
+const level25 = document.getElementById("level25");
+const level26 = document.getElementById("level26");
+const level27 = document.getElementById("level27");
+const level28 = document.getElementById("level28");
+const level1Option1 = document.getElementById("level1-option1");
+const level1Option2 = document.getElementById("level1-option2");
+const level2Option1 = document.getElementById("level2-option1");
+const level2Option2 = document.getElementById("level2-option2");
+const level3Option1 = document.getElementById("level3-option1");
+const level3Option2 = document.getElementById("level3-option2");
+const level4Option1 = document.getElementById("level4-option1");
+const level4Option2 = document.getElementById("level4-option2");
+const level5Option1 = document.getElementById("level5-option1");
+const level5Option2 = document.getElementById("level5-option2");
+const level6Option1 = document.getElementById("level6-option1");
+const level6Option2 = document.getElementById("level6-option2");
+const level7Option1 = document.getElementById("level7-option1");
+const level7Option2 = document.getElementById("level7-option2");
+const level8Option1 = document.getElementById("level8-option1");
+const level8Option2 = document.getElementById("level8-option2");
+const level9Option1 = document.getElementById("level9-option1");
+const level9Option2 = document.getElementById("level9-option2");
+const level10Option1 = document.getElementById("level10-option1");
+const level10Option2 = document.getElementById("level10-option2");
+const level11Option1 = document.getElementById("level11-option1");
+const level11Option2 = document.getElementById("level11-option2");
+const level12Option1 = document.getElementById("level12-option1");
+const level12Option2 = document.getElementById("level12-option2");
+const level13Option1 = document.getElementById("level13-option1");
+const level13Option2 = document.getElementById("level13-option2");
+const level14Option1 = document.getElementById("level14-option1");
+const level14Option2 = document.getElementById("level14-option2");
+const level15Option1 = document.getElementById("level15-option1");
+const level15Option2 = document.getElementById("level15-option2");
+const level16Option1 = document.getElementById("level16-option1");
+const level16Option2 = document.getElementById("level16-option2");
+const level17Option1 = document.getElementById("level17-option1");
+const level17Option2 = document.getElementById("level17-option2");
+const level18Option1 = document.getElementById("level18-option1");
+const level18Option2 = document.getElementById("level18-option2");
+const level19Option1 = document.getElementById("level19-option1");
+const level19Option2 = document.getElementById("level19-option2");
+const level20Option1 = document.getElementById("level20-option1");
+const level20Option2 = document.getElementById("level20-option2");
+const level21Option1 = document.getElementById("level21-option1");
+const level21Option2 = document.getElementById("level21-option2");
+const level22Option1 = document.getElementById("level22-option1");
+const level22Option2 = document.getElementById("level22-option2");
+const level23Option1 = document.getElementById("level23-option1");
+const level23Option2 = document.getElementById("level23-option2");
+const level24Option1 = document.getElementById("level24-option1");
+const level24Option2 = document.getElementById("level24-option2");
+const level25Option1 = document.getElementById("level25-option1");
+const level25Option2 = document.getElementById("level25-option2");
+const level26Option1 = document.getElementById("level26-option1");
+const level26Option2 = document.getElementById("level26-option2");
+const level27Option1 = document.getElementById("level27-option1");
+const level27Option2 = document.getElementById("level27-option2");
+const level28Option1 = document.getElementById("level28-option1");
+const level28Option2 = document.getElementById("level28-option2");
+
+// Tilføj klikbegivenheder til niveau 1-knapper
+level1Option1.addEventListener("click", () => {
+    // Skjul niveau 1
+    level1.style.display = "none";
+    // Vis niveau 2
+    level2.style.display = "block";
+});
+
+level1Option2.addEventListener("click", () => {
+});
+
+level2Option1.addEventListener("click", () => {
+    level2.style.display = "none";
+    level3.style.display = "block";
+});
+
+level2Option2.addEventListener("click", () => {
+});
+
+level3Option1.addEventListener("click", () => {
+    level3.style.display = "none";
+    level4.style.display = "block";
+});
+
+level3Option2.addEventListener("click", () => {
+});
+
+level4Option1.addEventListener("click", () => {
+    level4.style.display = "none";
+    level5.style.display = "block";
+});
+level5Option1.addEventListener("click", () => {
+    level5.style.display = "none";
+    level6.style.display = "block";
+});
+level6Option1.addEventListener("click", () => {
+    level6.style.display = "none";
+    level7.style.display = "block";
+});
+level7Option1.addEventListener("click", () => {
+    level7.style.display = "none";
+    level8.style.display = "block";
+});
+level8Option1.addEventListener("click", () => {
+    level8.style.display = "none";
+    level9.style.display = "block";
+});
+level9Option1.addEventListener("click", () => {
+    level9.style.display = "none";
+    level10.style.display = "block";
+});
+level10Option1.addEventListener("click", () => {
+    level10.style.display = "none";
+    level11.style.display = "block";
+});
+level11Option1.addEventListener("click", () => {
+    level11.style.display = "none";
+    level12.style.display = "block";
+});
+level12Option1.addEventListener("click", () => {
+    level12.style.display = "none";
+    level13.style.display = "block";
+});
+level13Option1.addEventListener("click", () => {
+    level13.style.display = "none";
+    level14.style.display = "block";
+});
+level14Option1.addEventListener("click", () => {
+    level14.style.display = "none";
+    level15.style.display = "block";
+});
+level15Option1.addEventListener("click", () => {
+    level15.style.display = "none";
+    level16.style.display = "block";
+});
+level16Option1.addEventListener("click", () => {
+    level16.style.display = "none";
+    level17.style.display = "block";
+});
+level17Option1.addEventListener("click", () => {
+    level17.style.display = "none";
+    level18.style.display = "block";
+});
+level18Option1.addEventListener("click", () => {
+    level18.style.display = "none";
+    level19.style.display = "block";
+});
+level19Option1.addEventListener("click", () => {
+    level19.style.display = "none";
+    level20.style.display = "block";
+});
+level20Option1.addEventListener("click", () => {
+    level20.style.display = "none";
+    level21.style.display = "block";
+});
+level21Option1.addEventListener("click", () => {
+    level21.style.display = "none";
+    level22.style.display = "block";
+});
+level22Option1.addEventListener("click", () => {
+    level22.style.display = "none";
+    level23.style.display = "block";
+});
+level23Option1.addEventListener("click", () => {
+    level23.style.display = "none";
+    level24.style.display = "block";
+});
+level24Option1.addEventListener("click", () => {
+    level24.style.display = "none";
+    level25.style.display = "block";
+});
+level25Option1.addEventListener("click", () => {
+    level25.style.display = "none";
+    level26.style.display = "block";
+});
+level26Option1.addEventListener("click", () => {
+    level26.style.display = "none";
+    level27.style.display = "block";
+});
+level27Option1.addEventListener("click", () => {
+    level27.style.display = "none";
+    level28.style.display = "block";
+});
+level28Option2.addEventListener("click", () => {
+    level28.style.display = "none";
+    level1.style.display = "block";
+});
